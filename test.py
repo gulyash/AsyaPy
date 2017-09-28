70409bd08ca9a02263d5810d8be610e943e1fd53
gulnara <igulnara6@gmail.com>
2017-09-28 19:55:16 +0500

    arithmetic test

---
+++ test.py
@@ -0,0 +1,23 @@
+print('Calculate 9 * 5. What is the answer?')
+answer = input()
+correctAnswer = 9 * 5
+print('Your input: ' + answer + '. Correct answer: ' + str(correctAnswer))
+
+print('Calculate 45 * 5. What is the answer?')
+answer = input()
+correctAnswer = 45 * 5
+print('Your input: ' + answer + '. Correct answer: ' + str(correctAnswer))
+
+print('Calculate 4.2 % 2. What is the answer?')
+answer = input()
+correctAnswer = 4.2 % 2
+print('Your input: ' + answer + '. Correct answer: ' + str(correctAnswer))
+
+print('Calculate 45 * 5 / 4.2 % 2. What is the answer?')
+answer = input()
+correctAnswer = 45 * 5 / 4.2 % 2
+print('Your input: ' + answer + '. Correct answer: ' + str(correctAnswer))
+
+print('Calculate 2 + 45 * 5 / 4.2 % 2. What is the answer?')
+answer = input()
+print('Your input: ' + answer + '. Correct answer: ' + str(2 + 45 * 5 / 4.2 % 2))
