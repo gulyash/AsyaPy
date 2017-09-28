f0f672d44b1b9d73881bd26db9cdb30d32fa1a5a
gulnara <igulnara6@gmail.com>
2017-09-28 22:58:43 +0500

    avgg

--- average.py
+++ average.py
@@ -3,7 +3,9 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-if a < b < c or c < b < a:
+if a == b or b == c or a == c:
+    print('Error')
+elif a < b < c or c < b < a:
     print(b)
 elif b < a < c or c < a < b:
     print(a)
