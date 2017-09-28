1382308271e42ac33219ee94483ca6e81ffc3bea
gulnara <igulnara6@gmail.com>
2017-09-28 22:53:02 +0500

    avg

---
+++ average.py
@@ -0,0 +1,11 @@
+print('Input 3 values:')
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a < b < c or c < b < a:
+    print(b)
+elif b < a < c or c < a < b:
+    print(a)
+else:
+    print(c)
