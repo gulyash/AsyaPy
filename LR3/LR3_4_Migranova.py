39f61719576064c7681cf04de043afcab7537380
RigDillinger <rigdillinger@yandex.ru>
2017-10-15 22:31:11 +0500

    removed isclose

--- LR3/LR3_4_Migranova.py
+++ LR3/LR3_4_Migranova.py
@@ -5,11 +5,11 @@ while True:
     print('Calculate 4.2 % 2. What is the answer?')
     answer = float(input())
     correctAnswer = 4.2 % 2
-    if math.isclose(answer, float(correctAnswer), abs_tol=0.0001):  # https://docs.python.org/3/library/math.html
+    if abs(answer-float(correctAnswer)) <= 0.0001:
         print('Your answer is correct.')
         break
     else:
         if answer < correctAnswer:
             print("Your answer is too small.")
         else:
-            print("Your answer is too bog.")
+            print("Your answer is too big.")
